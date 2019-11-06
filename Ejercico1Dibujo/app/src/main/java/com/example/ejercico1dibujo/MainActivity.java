bba2af82008311a6044a5031f36bf73b17b02b72
sandra1036 <sandraromero262@gmail.com>
2019-11-06 10:16:32 +0100

    Acabado ejercico circulo con cuadrados

--- Ejercico1Dibujo/app/src/main/java/com/example/ejercico1dibujo/MainActivity.java
+++ Ejercico1Dibujo/app/src/main/java/com/example/ejercico1dibujo/MainActivity.java
@@ -22,16 +22,46 @@ public class MainActivity extends AppCompatActivity {
             super(context);
         }
         protected void onDraw(Canvas canvas){
-            Paint paint=new Paint();
-            paint.setColor(Color.BLACK);
-            paint.setStrokeWidth(40);
-            paint.setStyle(Paint.Style.STROKE);
             int anchura=getWidth();
             int altura=getHeight();
+            Paint paint=new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.BLACK);
+            canvas.drawRect(0,0,anchura,altura,paint);
+
+
+            paint.setColor(Color.MAGENTA);
+            paint.setStrokeWidth(20);
+            paint.setStyle(Paint.Style.STROKE);
+
             String mensaje ="("+anchura+","+altura+")";
             paint.setTextSize(40);
-            canvas.drawText(mensaje,500,1000,paint);
-            canvas.drawCircle(500,500,80,paint);
+            canvas.drawCircle(540,792,400,paint);
+            paint.setStrokeWidth(100);
+            //canvas.drawText(mensaje,500,1000,paint);
+            paint.setColor(Color.RED);
+            canvas.drawRect(100,100,100,100,paint);
+            paint.setStrokeWidth(100);
+
+            paint.setColor(Color.BLUE);
+            canvas.drawRect(anchura-100,100,anchura-100,100,paint);
+            paint.setStrokeWidth(100);
+
+            paint.setColor(Color.YELLOW);
+            canvas.drawRect(anchura-100,anchura+200,anchura-100,anchura+200,paint);
+            paint.setStrokeWidth(100);
+
+            paint.setColor(Color.GREEN);
+            canvas.drawRect(100,anchura+200,100,anchura+200,paint);
+            paint.setStrokeWidth(100);
+
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setStrokeWidth(15);
+            canvas.drawRect(540,792,550,780,paint);
+
+
+
 
         }
     }
