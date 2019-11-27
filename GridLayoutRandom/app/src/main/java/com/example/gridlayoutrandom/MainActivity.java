4aae37083cb5558121bb998a50228f6134c9cc82
sandra1036 <sandraromero262@gmail.com>
2019-11-27 10:39:18 +0100

    Añadido Random a imagenes

--- GridLayoutRandom/app/src/main/java/com/example/gridlayoutrandom/MainActivity.java
+++ GridLayoutRandom/app/src/main/java/com/example/gridlayoutrandom/MainActivity.java
@@ -14,13 +14,14 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
     private int [] imageid={R.id.image1,R.id.image2,R.id.image3,R.id.image4,R.id.image5,
             R.id.image6,R.id.image7,R.id.image8,R.id.image9};
+
     private int [] imageViews={R.drawable.cochedosrueda,R.drawable.aeronave,R.drawable.bicicleta,
     R.drawable.doscanoas,R.drawable.moto,R.drawable.patinete,R.drawable.tren1,R.drawable.phaeton,R.drawable.submarino};
 
     private ImageView [] imagenes;
-    private Handler handler=new Handler();
 
     private ImageView imageresoult;
 
@@ -36,22 +37,16 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<9;i++){//coges los id de las imagenes
 
             imagenes[i]=(ImageView)findViewById(imageid[i]);
-            RandomUtlils.randomElement(imagenes);
-            RandomUtlils.randomInt(9);
-            imagenes[i].setImageResource(imageViews[i]);
-
-
-
+            imagenes[i].setImageResource(imageViews[RandomUtlils.randomInt(imageViews.length)]);
         }
 
         for(int i=0;i<9;i++){//Clickas en cada imagen
-
             final int finalI = i;
             imagenes[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String m="Has clickado=> "+imageViews[finalI];
-                    imageresoult.setImageResource(imageViews[finalI]);//Coge los drawables y lo ponen en ImageView para mostrar la imagen
+                    imageresoult.setImageResource(imageViews[RandomUtlils.randomInt(imageViews.length)]);//Coge los drawables y lo ponen en ImageView para mostrar la imagen lo hace aleatoriamente
                     Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
                 }
             });
@@ -62,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
 }
 class RandomUtlils{
-    private static Random r=new Random();
 
+    private static Random r=new Random();
     public static int randomInt(int range){
         return (r.nextInt(range));
     }
@@ -76,4 +71,5 @@ class RandomUtlils{
     public static float randomFloat(int n){
         return ((float)Math.random()*n);
     }
+
 }
