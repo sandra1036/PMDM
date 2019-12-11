1c9f992e1024b3d6de794926edad98a89c8ca9a6
sandra1036 <sandraromero262@gmail.com>
2019-12-11 10:42:35 +0100

    Finalizado FragmentoBoton

---
+++ FragmentoButton/app/src/main/java/com/example/fragmentobutton/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.fragmentobutton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Fragment;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private View frag;
+    private Button but;
+    private CheckBox ck;
+    int [] dibujos={R.drawable.capitanamarvel,R.drawable.spiderman,R.drawable.dr,R.drawable.ironman};
+    int stak;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ck=(CheckBox)findViewById(R.id.aparece);
+        frag=(View)findViewById(R.id.miFrg);
+
+        but=(Button)findViewById(R.id.buttonView);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment();
+            }
+        });
+
+
+        ck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(ck.isChecked()){
+                    frag.setVisibility(View.VISIBLE);
+                }else{
+                    frag.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+        if(savedInstanceState==null){//esto hace se apilen los fragmentos uno encima de otro y para saber la posicion
+            Fragment miFragment=MyFragment.newInstance(dibujos[0]);
+            FragmentTransaction fragmentTransaction=getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.miFrg,miFragment);
+            fragmentTransaction.commit();
+        }else{
+            stak=savedInstanceState.getInt("position");
+        }
+
+
+
+    }
+    public void addFragment(){
+        Fragment fragment;
+        Random random=new Random();
+        fragment=MyFragment.newInstance(dibujos[random.nextInt(dibujos.length)]);
+        FragmentTransaction tran=getFragmentManager().beginTransaction();
+        tran.replace(R.id.miFrg,fragment);
+        tran.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        tran.addToBackStack(null);
+        tran.commit();
+
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState){
+        super.onSaveInstanceState(outState,outPersistentState);
+        outState.putInt("position",stak);
+    }
+
+
+
+
+}
---
+++ FragmentoButton/app/src/main/java/com/example/fragmentobutton/MyFragment.java
@@ -0,0 +1,73 @@
+package com.example.fragmentobutton;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MyFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyFragment extends Fragment {
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private int mParam1;
+
+
+    public MyFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment MyFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyFragment newInstance(int param1) {
+        MyFragment fragment = new MyFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_my, container, false);
+        ImageView foto=(ImageView)v.findViewById(R.id.foto);
+        foto.setImageResource(mParam1);
+
+        return v;
+    }
+
+
+
+}
