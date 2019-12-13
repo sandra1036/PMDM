1b592f08d1d97000c2ca3649e77bd259e62352db
sandra1036 <sandraromero262@gmail.com>
2019-12-13 12:13:54 +0100

    Haciendo de manera diferente 2 Fragments

---
+++ FragmentosDinamicos/app/src/main/java/com/example/fragmentosdinamicos/Fragment1.java
@@ -0,0 +1,75 @@
+package com.example.fragmentosdinamicos;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Fragment1.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Fragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment1 extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+
+
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     *
+     * @return A new instance of fragment Fragment1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment1 newInstance(String param1) {
+        Fragment1 fragment = new Fragment1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v= inflater.inflate(R.layout.fragment_fragment1, container, false);
+        TextView texto;
+        texto=(TextView)v.findViewById(R.id.texto);
+        texto.setText("Fragmento num: ");
+        return v;
+    }
+
+
+}
